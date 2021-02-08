41d9c862725ceb198eb202f832e5d5f42e734400
mikeldiokelly <47454996+mikeldiokelly@users.noreply.github.com>
2021-02-08 17:14:36 +0000

    cheat google sign-in

---
+++ app/src/main/java/com/example/journey_sharing_12/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.journey_sharing_12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    public void onClickSignUp(View v){
+        int viewId =v.getId();
+        if(viewId ==R.id.signup){
+            Intent intent = new Intent(this,SignUpActivity.class);
+            startActivity(intent);
+
+        }
+    }
+
+    public void onClickLogin(View v){
+            int viewId = v.getId();
+            if(viewId == R.id.login){
+                EditText editText = findViewById(R.id.)
+            }
+        }
+
+
+}

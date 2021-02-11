e25ef360cd9dedcc96251d6c8cf373978934d7fe
caop <820672172@qq.com>
2021-02-11 02:05:47 +0000

    Working signin with Google (partially)

--- app/src/main/java/com/example/journey_sharing_12/MainActivity.java
+++ app/src/main/java/com/example/journey_sharing_12/MainActivity.java
@@ -10,8 +10,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -19,10 +26,17 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final int GOOGLE_SIGN_IN_CODE = 10005;
     private TextView register;
     private Button signIn;
     private EditText email, password;
     private FirebaseAuth mAuth;
+
+
+    SignInButton googleSignIn;
+    GoogleSignInOptions gso;
+    GoogleSignInClient signInClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstance){
@@ -34,26 +48,70 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         register = (TextView) findViewById(R.id.signup);
         register.setOnClickListener(this);
 
-        signIn = (Button) findViewById(R.id.button2);
+        signIn = (Button) findViewById(R.id.loginBtn);
         signIn.setOnClickListener(this);
 
         email = findViewById(R.id.editTextTextEmailAddress);
         password = findViewById(R.id.editTextTextPassword);
 
+
+        // google login
+        googleSignIn = findViewById(R.id.googleSignInBtn);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        signInClient = GoogleSignIn.getClient(this,gso);
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount!=null){
+            Toast.makeText(this,"User is logged in already",Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this,MapActivity.class));
+        }
+        googleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sign = signInClient.getSignInIntent();
+                startActivityForResult(sign, GOOGLE_SIGN_IN_CODE);
+            }
+        });
     }
 
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==GOOGLE_SIGN_IN_CODE){
+            Task<GoogleSignInAccount> signInTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount signInAcc = signInTask.getResult(ApiException.class);
+                Toast.makeText(this,"Google Account is connected to our application",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this,MapActivity.class));
+
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.signup:
+                Toast.makeText(this,"Y2",Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(this, SignUpActivity.class));
                 break;
-            case R.id.button2:
+            case R.id.loginBtn:
+                Toast.makeText(this,"2",Toast.LENGTH_SHORT).show();
                 userLogin();
                 break;
+
         }
     }
+
+
 
     private void userLogin() {
         String _email = email.getText().toString().trim();
@@ -83,6 +141,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
+
+
 
 
 }
